1f8d7d994a37c94ddbeb846c6d46ff23d0413e0a
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-09-28 17:39:29 +0300

    editing requirements file

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app,db
-from app.models import User
+from app.models import Blog, Comment, User
 from  flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
 
@@ -18,7 +18,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app=app,db=db,User=User,Comment=Comment,Blog=Blog)
 
 
 if __name__=='__main__':
